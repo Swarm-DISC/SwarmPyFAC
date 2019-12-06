d7c6170ebb8c3376fa5fbc9e08533950d8d45a20
asknevegamby <aknvg@space.dtu.dk>
2019-12-06 11:57:05 +0100

    removed some asserts testing input (asserts should only trigger when the code is wrong, not when the user is wrong)

--- swarmpyfac/utils.py
+++ swarmpyfac/utils.py
@@ -350,7 +350,6 @@ def delta(vectors):
     array([[ -1,   1,   3],
            [  7, -14,   1]])
     """
-    assert len(vectors) >= 2
     return vectors[1:] - vectors[:-1]
 
 
@@ -379,7 +378,6 @@ def means(vectors):
     array([[ 0.5,  0.5,  1.5],
            [ 3.5, -6. ,  3.5]])
     """
-    assert len(vectors) >= 2
     return (vectors[1:] + vectors[:-1]) * 0.5
 
 
@@ -451,7 +449,6 @@ def curl(delta_x, delta_field, target_index=2):
     >>> curl(pack_3d(np.ones((4,)),-1.5 * np.ones((4,)),np.zeros((4,))),vecs)
     array([5., 5., 5., 5.])
     """
-    assert target_index // 3 == 0
     index_x = (target_index+1) % 3
     index_y = (target_index+2) % 3
     # if delta_left is None:
